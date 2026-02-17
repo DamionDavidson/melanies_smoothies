b93749451f719d93eff36ae9b746b0f6449e42e2
DamionDavidson <damionrdavidson@gmail.com>
2026-02-17 09:09:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,13 @@ if ingredients_list and name_on_order:  # proceed only if user filled both
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + " " + 'Nutrition Information')
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        filtered = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON']
+
+   if not filtered.empty:
+        search_on = filtered.iloc[0]
+        st.write('The search value for', fruit_chosen, 'is', search_on, '.')
+   else:
+        st.write('No matching fruit found for', fruit_chosen)
         
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
