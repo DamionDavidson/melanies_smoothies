9a665921e1b56505a99d0ca26a692ef313c5deb8
DamionDavidson <damionrdavidson@gmail.com>
2026-02-16 16:02:44 -0800

    Update streamlit_app.py
    Hide JSON file ref. Add JSON into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,8 @@ elif ingredients_list and not name_on_order:
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
