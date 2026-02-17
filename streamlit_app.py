c7aae25a30620d250e2f7ee0766c1bd52b91ca0c
DamionDavidson <damionrdavidson@gmail.com>
2026-02-16 17:45:51 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ session = cnx.session()
 # Get fruit list from Snowflake table
 fruit_df = session.table("smoothies.public.fruit_options").select(col("fruit_name"),col('SEARCH_ON'))
 fruit_rows = fruit_df.collect()
-fruit_list = [row["FRUIT_NAME"] for row in fruit_rows]
-st.stop()
+fruit_list = [row["SEARCH_ON"] for row in fruit_rows]
+
 
 
 
