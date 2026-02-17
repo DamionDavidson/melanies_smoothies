8060edb83a054ef863047c38129eb8fcab2dac8b
DamionDavidson <damionrdavidson@gmail.com>
2026-02-17 09:02:46 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ fruit_list = [row["SEARCH_ON"] for row in fruit_rows]
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC funtion
 pd_df=fruit_df.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+
 
 # Multiselect for ingredients
 ingredients_list = st.multiselect(
