48988b4502a5578bb42cd91f6890db92f89a6e04
DamionDavidson <damionrdavidson@gmail.com>
2026-02-16 16:11:58 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ ingredients_list = st.multiselect(
 if ingredients_list and name_on_order:  # proceed only if user filled both
     ingredients_string = " ".join(ingredients_list)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     if st.button("Submit Order"):
