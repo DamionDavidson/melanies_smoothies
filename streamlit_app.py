d447e4b33e66db02e674107bbe9f3c6e81a82431
DamionDavidson <damionrdavidson@gmail.com>
2026-02-20 11:32:32 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if ingredients_list and name_on_order:
 
     for fruit_name in ingredients_list:
 
-        st.subheader(f"{fruit_name} Nutrition Information")
+        st.subheader(f"{search_on} Nutrition Information")
 
         # Match on SEARCH_ON since that's what your multiselect uses
         filtered = pd_df.loc[
