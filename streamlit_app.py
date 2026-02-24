8eeecfe8010450d6fa223eed04e905343ae53512
DamionDavidson <damionrdavidson@gmail.com>
2026-02-24 14:01:24 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ ingredients_list = st.multiselect(
 
 if ingredients_list and name_on_order:
 
-    ingredients_string = " ".join(ingredients_list)
+    ingredients_string = "".join(ingredients_list)
 
     for fruit in ingredients_list:
 
